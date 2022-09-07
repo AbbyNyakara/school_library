2ea4d1b32622347579e91e19bf66056969eeceb4
Abby Nyakara <abbynyakara@gmail.com>
2022-09-07 11:18:10 +0300

    add decorator

--- person.rb
+++ person.rb
@@ -1,20 +1,28 @@
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
+end
+
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  # Getter method for id
-  attr_reader :id
+  def correct_name
+    @name
+  end
 
-  # Getter and setter method for name, age
-  attr_accessor :name, :age
-
-  # define the public method first
+  # define the public method
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
   # define private method is_of_age
@@ -22,5 +30,38 @@ class Person
     @age >= 18
   end
 
-  private :is_of_age?
+  private :of_age?
 end
+
+
+class NameableBaseDecorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < NameableBaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < NameableBaseDecorator
+  def correct_name
+    @nameable.correct_name
+    @nameable.correct_name[0...10] if @nameable.correct_name.length > 10
+  end
+end
+
+person = Person.new(22, 'maximilianus')
+  person.correct_name
+  capitalizedPerson = CapitalizeDecorator.new(person)
+  capitalizedPerson.correct_name
+  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+  capitalizedTrimmedPerson.correct_name
